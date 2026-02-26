939ae6005012646e113dc22b6fca6005fc6b2dc6
axc-project <avi@axellaresearch.com>
2026-02-26 08:56:48 -0500

    Change output type to 'hybrid' and add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid',
   site: 'https://historicaltranslationproject.com',
+  adapter: vercel(),
   integrations: [
     tailwind(),
     sitemap(),
