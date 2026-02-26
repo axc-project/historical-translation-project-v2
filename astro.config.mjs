e85c1b2747ce0e2c1ee4bf16d7a888b6a63e3141
axc-project <avi@axellaresearch.com>
2026-02-26 09:18:37 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   site: 'https://historicaltranslationproject.com',
   adapter: vercel(),
   integrations: [
